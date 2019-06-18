6dd53c4228107c08c40efd2538876d96ab97b9de
Yang Jie <jiyang@thoughtworks.com>
2019-06-18 20:49:11 +0800

    Alter: add unit tests and before KeysFilter

--- Kata/Refactor/After/KeysFilter.cs
+++ Kata/Refactor/After/KeysFilter.cs
@@ -1,49 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Kata.Refactor.After
 {
-    using System.Collections.Generic;
-    using System.Linq;
-
-    namespace Kata.Refactor.Before
+    public class KeysFilter
     {
-        public class KeysFilter
+        private ISessionService SessionService { get; set; }
+
+        private readonly MarksHelper _marksHelper = new MarksHelper();
+
+        public List<string> Filter(IList<string> marks, bool isGoldenKey)
         {
-            private ISessionService SessionService { get; set; }
-            
-            private readonly MarksHelper _marksHelper = new MarksHelper();
-
-            public List<string> Filter(IList<string> marks, bool isGoldenKey)
+            if (marks == null && marks.Count == 0)
             {
-                if (marks != null && marks.Count > 0)
-                {
-                    return new List<string>();
-                }
-
-                if (isGoldenKey)
-                {
-                    var goldenMarks = _marksHelper.GetGoldenMarks(marks);
-                    
-                    var goldenMarksFilterByKeys = GetMarksFilterByKeys(goldenMarks, FilterKey.GoldenKey);
-
-                    return goldenMarksFilterByKeys;
-                }
-
-                var silverMarksFilterByKeys = GetMarksFilterByKeys(marks, FilterKey.SilverKey);
-
-                var copperMarksFilterByKeys = GetMarksFilterByKeys(marks, FilterKey.CopperKey);
-
-                var otherMarks = silverMarksFilterByKeys.Concat(copperMarksFilterByKeys).ToList();
-                
-                return otherMarks;
+                return new List<string>();
             }
 
-            private List<string> GetMarksFilterByKeys(IEnumerable<string> marks, FilterKey filter)
+            if (isGoldenKey)
             {
-                var silverKeys = SessionService.Get<List<string>>(filter.ToString());
+                var goldenMarks = _marksHelper.GetGoldenMarks(marks);
 
-                var keys = new List<string>(silverKeys);
+                var goldenMarksFilterByKeys = GetMarksFilterByKeys(goldenMarks, FilterKey.GoldenKey);
 
-                return marks.Where(mark => keys.Contains(mark) || _marksHelper.IsFakeMark(mark)).ToList();
+                return goldenMarksFilterByKeys;
             }
+
+            var silverMarksFilterByKeys = GetMarksFilterByKeys(marks, FilterKey.SilverKey);
+
+            var copperMarksFilterByKeys = GetMarksFilterByKeys(marks, FilterKey.CopperKey);
+
+            var otherMarks = silverMarksFilterByKeys.Concat(copperMarksFilterByKeys).ToList();
+
+            return otherMarks;
+        }
+
+        private List<string> GetMarksFilterByKeys(IEnumerable<string> marks, FilterKey filter)
+        {
+            var silverKeys = SessionService.Get<List<string>>(filter.ToString());
+
+            var keys = new List<string>(silverKeys);
+
+            return marks.Where(mark => keys.Contains(mark) || _marksHelper.IsFakeMark(mark)).ToList();
         }
     }
 }
